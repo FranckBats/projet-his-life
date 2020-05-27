e90c5dbe29c970daf7fbb140195af7e256f9221f
Audrey78 <abodin78250@gmail.com>
2020-05-27 17:17:13 +0200

    DashboardLoginRegisterTwig

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -12,32 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PeopleController extends AbstractController
 {
     
-    /**
-     * @Route("/people/register", name="people_register")
-     */
-    public function register(Request $request ) 
-    {
-        $people = new People();
-
-        $form = $this->createForm(RegisterType::class, $people);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($people);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Vous êtes enregistré. Vous pouvez maintenant vous connecter.');
-
-            //return $this->redirectToRoute('a_definir');
-        }
-
-
-        return $this->render('people/register.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
+    
     /**
      * @Route("/people/profile", name="people_profile")
      */
