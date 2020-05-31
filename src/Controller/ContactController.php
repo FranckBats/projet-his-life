477a384bec86f626e27e5b285220fdc972b7b255
FlorianeGitton <gitton.floriane@gmail.com>
2020-05-31 15:18:49 +0200

    contact ALMOST finish for back-end

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,100 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Form\DeleteType;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/contact", name="contact_browse")
      */
-    public function index()
+    public function Browse(ContactRepository $contactRepository)
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+        return $this->render('contact/browse.html.twig', [
+            'contacts' => $contactRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/contact/edit/{id}", name="contact_edit", requirements={"id": "\d+"})
+     */
+    public function Edit(Contact $contact, Request $request)
+    {
+        
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contact->setUpdatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($contact);
+
+            $em->flush();
+
+            return $this->redirectToRoute('contact_browse');
+
+        }
+
+        return $this->render('contact/edit.html.twig', [
+            'form' => $form->createView(), 
+        ]);
+    }
+
+    /**
+     * @Route("/contact/add", name="contact_add")
+     */
+    public function add(Request $request)
+    {
+        $contact = New Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($contact);
+
+            $em->flush();
+
+            return $this->redirectToRoute('contact_browse');
+
+
+        }
+
+        return $this->render('contact/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("contact/delete/{id}", name="contact_delete", requirements={"id": "\d+"}, methods={"DELETE"})
+     */
+    public function delete(EntityManagerInterface $em, Contact $contact, Request $request)
+    {
+        $formDelete = $this->createForm(DeleteType::class);
+
+        $formDelete->handleRequest($request);
+
+        if ($formDelete->isSubmitted() && $formDelete->isValid()) {
+            $em->remove($contact);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('contact_browse');
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,41 +2,37 @@
 
 namespace App\Form;
 
-
+use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
-
-class UploadType extends AbstractType
+class ContactType extends AbstractType
 {
-    
-    
-
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {   
-       
-        
+    {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Nom'
+            ->add('name')
+            ->add('job')
+            ->add('address')
+            ->add('phone')
+            ->add('email')
+            ->add('family')
+            ->add('submit', SubmitType::class, [
+                'label' => 'enregistrer '
             ])
-            ->add('file', FileType::class, [
-                'label' => 'Photo'
+            ->setMethod('DELETE')
+            ->add('deleteButton', SubmitType::class,[
+                'label' => 'Supprimer',
             ])
-            ->add('submit', SubmitType::class)
-            
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Contact::class,
         ]);
     }
 }
